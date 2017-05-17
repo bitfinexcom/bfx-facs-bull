23ad203fadec7d55449cbb9cb6a6dc6788214d5a
prdn <paolo.ardoino@gmail.com>
2017-05-17 09:41:44 +0000

    add control feature

--- bull.js
+++ bull.js
@@ -31,22 +31,24 @@ class BullFacility extends Facility {
           queue: this.opts.queue
         }))
 
-        this.queue.on('cleaned', (jobs, type) => {
-          if (this.opts.debug) {
-            console.log('cleaned %s %s jobs', jobs.length, type)
-          }
-        })
-
-        this._itvCount = setInterval(() => {
-          this.queue.count().then(cnt => {
-            if (cnt) console.log('bull count', cnt)
+        if (this.opts.control) {
+          this.queue.on('cleaned', (jobs, type) => {
+            if (this.opts.debug) {
+              console.log('cleaned %s %s jobs', jobs.length, type)
+            }
           })
-        }, 30000)
 
-        this._itv = setInterval(() => {
-          this.queue.clean(10000, 'completed', 50000)
-          this.queue.clean(10000, 'failed', 50000)
-        }, 60000)
+          this._itvCount = setInterval(() => {
+            this.queue.count().then(cnt => {
+              if (cnt) console.log('bull count', cnt)
+            })
+          }, 30000)
+
+          this._itv = setInterval(() => {
+            this.queue.clean(10000, 'completed', 50000)
+            this.queue.clean(10000, 'failed', 50000)
+          }, 60000)
+        }
 
         next()
       }
