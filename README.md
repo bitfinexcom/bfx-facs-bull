379593a957ba52077574a404e02992221ca6e155
prdn <paolo.ardoino@gmail.com>
2017-12-02 18:43:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# bfx-facs-base
+
+git clone git@github.com:bitfinexcom/REPO.git REPO
+
+git remote -v
+
+git remote add upstream git@github.com:bitfinexcom/PARENT.git
+
+git remote -v
+
+git push origin master
---
+++ base.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const EventEmitter = require('events')
+const fs = require('fs')
+const _ = require('lodash')
+const async = require('async')
+
+class Facility extends EventEmitter {
+  constructor (caller, opts, ctx) {
+    super()
+
+    this.name = 'facility'
+    this.caller = caller
+    this.opts = _.extend({ ns: this.name }, opts)
+    this.ctx = ctx
+  }
+
+  init () {
+    if (this._hasConf) {
+      const cal = this.caller
+
+      const conf = JSON.parse(
+        fs.readFileSync(
+          `${cal.ctx.root}/config/facs/${this.name}.config.json`, 'utf8'
+        )
+      )
+      this.conf = conf[this.opts.ns]
+    }
+  }
+
+  set (k, v) {
+    this[k] = v
+  }
+
+  start (cb) {
+    async.series([
+      next => {
+        this._start0(next)
+      },
+      next => {
+        this.active = 1
+        next()
+      },
+      next => {
+        this._start(next)
+      }
+    ], cb)
+  }
+
+  _start0 (cb) { cb() }
+  _start (cb) { cb() }
+
+  stop (cb) {
+    async.series([
+      next => {
+        this._stop(next)
+      },
+      next => {
+        this.active = 0
+        if (!this.working) return next()
+
+        let itv = setInterval(() => {
+          if (this.working) return
+          clearInterval(itv)
+          next()
+        }, 1000)
+      },
+      next => {
+        this._stop9(next)
+      }
+    ], cb)
+  }
+
+  _stop (cb) { cb() }
+  _stop9 (cb) { cb() }
+}
+
+module.exports = Facility
